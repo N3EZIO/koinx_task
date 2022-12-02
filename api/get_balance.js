f7330cd88e2aeba75255330b898fdba76a3ec4d7
Nitin Arul <nitinarul68@gmail.com>
2022-12-03 02:40:19 +0530

    completed task2 and task3

--- api/get_balance.js
+++ api/get_balance.js
@@ -1,12 +1,73 @@
-const getBalance = async (address) => {
-    const response = await axios.get('https://api.etherscan.io/api', {
-        params: {
-            module: 'account',
-            action: 'balance',
-            address: address,
-            tag: 'latest',
-            apikey: config.apiKey
+const getBalance = async (transactions, address) => {
+    
+    let balance = 0;
+    for(let i=0; i<transactions.length; i++){
+        const value = transactions[i].value / 1000000000000000000;
+        if(transactions[i].from === address){
+            balance -= value;
         }
-    });
-    return response.data.result;
+        if(transactions[i].to === address){
+            balance += value;
+        }
+    }
+
+    return balance;
+
 }
+
+const express = require('express');
+const axios = require('axios');
+const Router = express.Router();
+const config = require('../config');
+const User = require('../models/transactions');
+const Price = require('../models/price');
+
+Router.get('/:address', async (req, res) => {
+    const address = req.params.address;
+
+    const price = await Price.findOne({});
+    //check if address is valid hexademical
+    if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+        return res.status(400).json({message: 'Invalid address'});
+    }
+    try{
+        //get transactions from db using address
+        const user = await User.findOne({address: address});
+        if(user){
+            const transactions = user.transactions;
+            const balance = await getBalance(transactions, address);
+            res.status(200).json({balance: balance, price: price.price});
+        }else{
+            //send a get request to the blockchain api and insert the data into the database
+            const response = await axios.get('https://api.etherscan.io/api',{
+                params: {
+                    module: 'account',
+                    action: 'txlist',
+                    address: address,
+                    startblock: 0,
+                    endblock: 99999999,
+                    sort: 'asc',
+                    apikey: config.apiKey
+                }
+            })
+
+            const transactions = response.data.result;
+            const balance = await getBalance(transactions, address);
+            res.status(200).json({balance: balance, price: price.price});
+            const user = new User({
+                address: address,
+                transactions: transactions
+            });
+            await user.save();
+        }
+
+    }catch(err){
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+        
+})
+
+
+module.exports = Router;
--- api/normal_txn.js
+++ api/normal_txn.js
@@ -6,10 +6,14 @@ const User = require('../models/transactions');
 
 Router.get('/:address', async (req, res) => {
     const address = req.params.address;
+    //check if address is valid hexademical
+    if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+        return res.status(400).json({message: 'Invalid address'});
+    }
     try{
         
             //send a get request to the blockchain api and insert the data into the database
-            const response = axios.get('https://api.etherscan.io/api',{
+            const response = await axios.get('https://api.etherscan.io/api',{
                 params: {
                     module: 'account',
                     action: 'txlist',
@@ -25,32 +29,29 @@ Router.get('/:address', async (req, res) => {
             res.json(transactions);
             const user = new User({
                 address: address,
-                transactions: transactions
+                transactions: transactionss
             });
 
 
             //check if the user already exists in the database and update the transactions else insert the user
             const userExists = await User.findOne({address: address});
             if(userExists){
-                user.findOneAndUpdate({address: address, transactions: transactions}, {new: true}, (err, doc) => {
-                    if(err){
-                        console.log(err);
-                    }
-                    console.log(doc);
+                if(userExists.transactions.length !== transactions.length){
+                    await user.findOneAndUpdate({address: address, transactions: transactions}, {new: true}); 
                 }
-                )
             }else{
-                user.save();
+                await user.save();
             }
 
 
 
     }
     catch(err){
-        res.status(500).json({
-            message: err.message
-        });
+        res.status(500)
     }
 
 
 });
+
+
+module.exports = Router;
