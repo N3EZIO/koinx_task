e38bdacf11f4571648a809a927d1212ca3cf0b8b
Nitin Arul <nitinarul68@gmail.com>
2022-12-02 21:13:18 +0530

    changes made to task1 to accomodate another case

--- api/normal_txn.js
+++ api/normal_txn.js
@@ -7,13 +7,8 @@ const User = require('../models/transactions');
 Router.get('/:address', async (req, res) => {
     const address = req.params.address;
     try{
-        const user = await User.findOne({address: address});
-        if(user){
-            //return all the txn records from the db if it already exists
-            res.json(user.transactions);
-        }else{
+        
             //send a get request to the blockchain api and insert the data into the database
-
             const response = axios.get('https://api.etherscan.io/api',{
                 params: {
                     module: 'account',
@@ -27,13 +22,29 @@ Router.get('/:address', async (req, res) => {
             })
 
             const transactions = response.data.result;
-
+            res.json(transactions);
             const user = new User({
                 address: address,
                 transactions: transactions
             });
 
-        }
+
+            //check if the user already exists in the database and update the transactions else insert the user
+            const userExists = await User.findOne({address: address});
+            if(userExists){
+                user.findOneAndUpdate({address: address, transactions: transactions}, {new: true}, (err, doc) => {
+                    if(err){
+                        console.log(err);
+                    }
+                    console.log(doc);
+                }
+                )
+            }else{
+                user.save();
+            }
+
+
+
     }
     catch(err){
         res.status(500).json({
